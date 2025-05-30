c1d7fff7830ce936ee96c081c5a26b0abb183402
Asterikss <AndreAndreM@proton.me>
2025-05-30 13:09:58 +0200

    add postgres setup in devenv with pgvector

--- devenv.nix
+++ devenv.nix
@@ -15,4 +15,16 @@
       sync.enable = true;
     };
   };
+  packages = [
+    pkgs.atlas
+  ];
+  services.postgres = {
+    enable = true;
+    listen_addresses = "localhost";
+    port = 5432;
+    initialDatabases = [ { name = "rtfm-rag"; } ];
+    extensions = extensions: [
+      extensions.pgvector
+    ];
+  };
 }
