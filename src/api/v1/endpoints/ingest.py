74121c556b1831062bc2a962e5348e3d67c95a15
Asterikss <AndreAndreM@proton.me>
2025-06-06 00:20:54 +0200

    add a response_model to the ingest endpoint

--- src/api/v1/endpoints/ingest.py
+++ src/api/v1/endpoints/ingest.py
@@ -21,7 +21,15 @@ class IngestLinkSchema(BaseModel):
   max_pages: Annotated[int, Field(strict=True, gt=0)]
 
 
-async def _ingest_link(ingest_link_schema: IngestLinkSchema) -> Result[Dict, str]:
+class IngestLinkResponseSchema(BaseModel):
+  scraping_summary: Dict[str, Any]
+  storage_summary: Dict[str, Any]
+  status: str
+
+
+async def _ingest_link(
+  ingest_link_schema: IngestLinkSchema,
+) -> Result[IngestLinkResponseSchema, str]:
   scraper_config = ScraperConfig(
     max_depth=ingest_link_schema.max_depth, max_pages=ingest_link_schema.max_pages
   )
@@ -37,19 +45,21 @@ async def _ingest_link(ingest_link_schema: IngestLinkSchema) -> Result[Dict, str
   if isinstance(data_storage_result, Err):
     return Err(data_storage_result.err())
 
-  combined_summary = {
-    "scraping": scrape_result.ok(),
-    "storage": data_storage_result.ok().model_dump(),
-    "status": "complete",
-  }
+  return Ok(
+    IngestLinkResponseSchema(
+      scraping_summary=scrape_result.ok(),
+      storage_summary=data_storage_result.ok().model_dump(),
+      status="complete",
+    )
+  )
 
-  return Ok(combined_summary)
 
-
-@router.post("/link")
-async def ingest_link(ingest_link_schema: IngestLinkSchema) -> Dict[str, Any]:
+@router.post("/link", response_model=IngestLinkResponseSchema)
+async def ingest_link(ingest_link_schema: IngestLinkSchema):
   try:
-    result: Result[Dict, str] = await _ingest_link(ingest_link_schema)
+    result: Result[IngestLinkResponseSchema, str] = await _ingest_link(
+      ingest_link_schema
+    )
     match result:
       case Ok(summary):
         return summary
