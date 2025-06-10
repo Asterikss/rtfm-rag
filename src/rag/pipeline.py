76d680f75a3f4c870c1e435f8973acb00297413c
Asterikss <AndreAndreM@proton.me>
2025-06-10 17:42:12 +0200

    significantly improve the rag pipeline file (nice)

--- src/rag/pipeline.py
+++ src/rag/pipeline.py
@@ -1,11 +1,6 @@
-from typing import List
+from typing import List, TYPE_CHECKING
 
-from openai import OpenAI
-import psycopg
-from result import Err, Ok, Result
-
-from .embedder import embed_data
-from .generator import generate_response
+from result import Err, Ok, Result, UnwrapError
 
 from ..api.v1.schemas import MessageResponseSchema, MessageSchema
 from ..core.constants import rag
@@ -13,49 +8,40 @@ from ..repositories.chunk_repository import ChunkRetriveData, find_closest_chunk
 from ..repositories.index_repository import get_chunk_id_by_name
 from ..services.database_service import get_database_connection
 from ..services.openai_service import get_openai_client
+from .embedder import embed_data
+from .generator import generate_response
+
+if TYPE_CHECKING:
+  from openai import OpenAI
+  import psycopg
 
 
 def rag_pipeline(
   message_schema: MessageSchema,
 ) -> Result[MessageResponseSchema, str]:
-  db_conn_result: Result[psycopg.Connection, str] = get_database_connection()
-  if isinstance(db_conn_result, Err):
-    return Err(db_conn_result.err())
+  try:
+    db_conn: psycopg.Connection = get_database_connection().unwrap()
 
-  get_chunk_id_result: Result[int, str] = get_chunk_id_by_name(
-    db_conn_result.ok(), message_schema.indexName
-  )
-  if isinstance(get_chunk_id_result, Err):
-    return Err(get_chunk_id_result.err())
+    get_chunk_id: int = get_chunk_id_by_name(db_conn, message_schema.indexName).unwrap()
 
-  openai_client_result: Result[OpenAI, str] = get_openai_client()
-  if isinstance(openai_client_result, Err):
-    return Err(openai_client_result.err())
+    openai_client: OpenAI = get_openai_client().unwrap()
 
-  embed_result: Result[List[float], str] = embed_data(
-    openai_client_result.ok(), message_schema.text
-  )
-  if isinstance(embed_result, Err):
-    return Err(embed_result.err())
+    embedding: List[float] = embed_data(openai_client, message_schema.text).unwrap()
 
-  retrived_chunks_result: Result[List[ChunkRetriveData], str] = find_closest_chunks(
-    db_conn_result.ok(), embed_result.ok(), get_chunk_id_result.ok()
-  )
-  if isinstance(retrived_chunks_result, Err):
-    return Err(retrived_chunks_result.err())
+    retrived_chunks: List[ChunkRetriveData] = find_closest_chunks(
+      db_conn, embedding, get_chunk_id
+    ).unwrap()
 
-  filtered_chunks: List[ChunkRetriveData] = [
-    chunk_data
-    for chunk_data in retrived_chunks_result.ok()
-    if chunk_data.distance < rag.MAX_RELEVANT_DISTANCE
-  ]
+    filtered_chunks: List[ChunkRetriveData] = [
+      chunk_data
+      for chunk_data in retrived_chunks
+      if chunk_data.distance < rag.MAX_RELEVANT_DISTANCE
+    ]
 
-  response_result: Result[str, str] = generate_response(
-    message_schema.text, filtered_chunks
-  )
-  if isinstance(response_result, Err):
-    return Err(response_result.err())
+    response: str = generate_response(message_schema.text, filtered_chunks).unwrap()
 
-  links: List[str] = list(set(chunk_data.url for chunk_data in filtered_chunks))
+    links: List[str] = list(set(chunk_data.url for chunk_data in filtered_chunks))
 
-  return Ok(MessageResponseSchema(text=response_result.ok(), links=links))
+    return Ok(MessageResponseSchema(text=response, links=links))
+  except UnwrapError as e:
+    return Err(str(e))
