eeddc611abba4de705161b81f2eba37db1eb200f
Asterikss <AndreAndreM@proton.me>
2025-06-27 14:01:39 +0200

    add get_time and get_time_async helper functions

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,4 +1,6 @@
-from functools import lru_cache
+from functools import lru_cache, wraps
+from time import perf_counter
+from typing import Any, Callable
 
 import tiktoken
 
@@ -12,3 +14,28 @@ def _get_embed_model_encoding():
 
 def get_embed_token_count(text: str) -> int:
   return len(_get_embed_model_encoding().encode(text))
+
+
+def get_time(func: Callable) -> Callable:
+  @wraps(func)
+  def wrapper(*args, **kwargs) -> Any:
+    start_time: float = perf_counter()
+    result: Any = func(*args, **kwargs)
+    end_time: float = perf_counter()
+
+    print(f"'{func.__name__}()' took {end_time - start_time:.3f} seconds to execute")
+    return result
+
+  return wrapper
+
+
+def get_time_async(func: Callable) -> Callable:
+  @wraps(func)
+  async def wrapper(*args, **kwargs) -> Any:
+    start_time = perf_counter()
+    result = await func(*args, **kwargs)
+    end_time = perf_counter()
+    print(f'"{func.__name__}()" took {end_time - start_time:.3f} seconds to execute')
+    return result
+
+  return wrapper
