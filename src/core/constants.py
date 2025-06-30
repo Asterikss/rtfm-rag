fea6e3c1eeb67875d2d16297089f33b42d963189
Asterikss <AndreAndreM@proton.me>
2025-07-01 00:28:54 +0200

    update constants and response generation

--- src/core/constants.py
+++ src/core/constants.py
@@ -4,6 +4,8 @@ from typing import NamedTuple
 class RagNamespace(NamedTuple):
   MAX_RELEVANT_DISTANCE: float
   EMBEDDING_MODEL: str
+  EMBEDDING_TOKEN_LIMIT: int
+  GENERATOR_MODEL: str
   GENERATOR_SYSTEM_PROMPT: str
   GENERATOR_USER_PROMPT_TEMPLATE: str
 
@@ -11,37 +13,43 @@ class RagNamespace(NamedTuple):
 rag = RagNamespace(
   MAX_RELEVANT_DISTANCE=1.0,
   EMBEDDING_MODEL="text-embedding-3-small",
+  EMBEDDING_TOKEN_LIMIT=8192,
+  GENERATOR_MODEL="gpt-4.1-nano-2025-04-14",
   GENERATOR_SYSTEM_PROMPT="""
-  You are an expert assistant specialized in providing precise, clear, and
-  concise explanations based on technical documentation. You will be given a
-  user query along with relevant excerpts from official documentation retrieved
-  specifically to answer the query. Importantly remember that retrieved data in
-  the context might not contain answers to the query. Also keep in mind that
-  the query might not be connected to a technical documentation. In that case
-  proceed normally.
+You are an expert assistant specialized in providing precise, clear, and
+concise explanations based on technical documentation. You will be given a
+user query along with relevant excerpts from official documentation retrieved
+specifically to answer the query. Importantly remember that retrieved data in
+the context might not contain answers to the query. Also keep in mind that
+the query might not be connected to a technical documentation. In that case
+proceed normally.
 
-  Instructions:
-  - Try to use the provided documentation excerpts as your source of information.
-  - If the answer is directly found in the provided context, respond by
-  paraphrasing or summarizing that information clearly.
-  - If the context does not contain an answer, and you do not know the answer
-  based on your own knowladge, reply politely that the information is not available in the
-  retrieved documentation. If you know the answer to the qustion yourself, try your best
-  to respond to the query, but notify the user that you are using your own
-  knowladge, if that is the case.
-  - When explaining concepts, prioritize clarity and accuracy over verbosity.
-  - Provide example code snippets if relevant and present in the context.
-  - Tailor the language for developers familiar with technical documentation,
-  but avoid unnecessary jargon.
-  - Structure your answer logically and clearly.
+Instructions:
+- Try to use the provided documentation excerpts as your source of information.
+- If the answer is directly found in the provided context, respond by
+paraphrasing or summarizing that information clearly.
+- If the context does not contain an answer, and you do not know the answer
+based on your own knowladge, reply politely that the information is not available in the
+retrieved documentation. If you know the answer to the qustion yourself, try your best
+to respond to the query, but notify the user that you are using your own
+knowladge, if that is the case.
+- When explaining concepts, prioritize clarity and accuracy over verbosity.
+- Provide example code snippets if relevant and present in the context.
+- Tailor the language for developers familiar with technical documentation,
+but avoid unnecessary jargon.
+- Structure your answer logically and clearly.
+- Try to utilize markdown where it make sense to use it. Do not overdo
+heading, keep it lean. Importantly, use new lines to clearly break up content
+where needed. Any code blocks should be clearly separated and markdown syntax
+for code blocks should be used.
   """,
   GENERATOR_USER_PROMPT_TEMPLATE=r"""
-  <CONTEXT>
-  {context}
-  <\CONTEXT>
+<CONTEXT>
+{context}
+<\CONTEXT>
 
-  <USER_QUERY>
-  {user_query}
-  <\USER_QUERY>
+<USER_QUERY>
+{user_query}
+<\USER_QUERY>
   """,
 )
--- src/rag/generator.py
+++ src/rag/generator.py
@@ -1,16 +1,19 @@
 from typing import List
+
 from result import Err, Ok, Result
 
+from ..core.constants import rag
 from ..repositories.chunk_repository import ChunkRetriveData
 from ..services.openai_service import get_openai_client
-from ..core.constants import rag
+from ..utils.utils import get_time
 
 
+@get_time
 def generate_response(query: str, chunks: List[ChunkRetriveData]) -> Result[str, str]:
   # TODO: possibly utilize links
   openai_clinet_result = get_openai_client()
   if isinstance(openai_clinet_result, Err):
-    return Err(openai_clinet_result.err())
+    return openai_clinet_result
 
   context_list: List[str] = []
   for chunk in chunks:
@@ -24,10 +27,10 @@ def generate_response(query: str, chunks: List[ChunkRetriveData]) -> Result[str,
 
   try:
     response = openai_clinet_result.ok().responses.create(
-      model="gpt-4.1-mini",
+      model=rag.GENERATOR_MODEL,
       temperature=0.2,
       instructions=rag.GENERATOR_SYSTEM_PROMPT,
-      max_output_tokens=1000,
+      max_output_tokens=1500,
       input=[
         {
           "role": "user",
@@ -37,4 +40,4 @@ def generate_response(query: str, chunks: List[ChunkRetriveData]) -> Result[str,
     )
     return Ok(response.output_text)
   except Exception as e:
-    return Err(f"Exception occured when trying to generate an llm answer: {e}")
+    return Err(f"Exception occurred when trying to generate an llm answer: {e}")
