37bc9978e7817f741b714fa3bfd893e4d9928aee
Asterikss <AndreAndreM@proton.me>
2025-07-02 20:48:23 +0200

    rewrite ingest endpoint to use connection pool

--- src/api/v1/endpoints/ingest.py
+++ src/api/v1/endpoints/ingest.py
@@ -1,11 +1,17 @@
-from typing import Any, Dict, Annotated
+from __future__ import annotations
+from typing import Annotated, Any, Dict, TYPE_CHECKING
 
-from fastapi import APIRouter, HTTPException
-from pydantic import BaseModel, HttpUrl, Field, StringConstraints
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel, Field, HttpUrl, StringConstraints
 from result import Err, Ok, Result
+
+from src.services.database_service import get_db_conn
 
 from ....services.documentation_scraper import DocumentationScraper, ScraperConfig
 from ....services.store_data import store_data
+
+if TYPE_CHECKING:
+  from psycopg import AsyncConnection
 
 
 router = APIRouter(prefix="/ingest")
@@ -28,20 +34,20 @@ class IngestLinkResponseSchema(BaseModel):
 
 
 async def _ingest_link(
-  ingest_link_schema: IngestLinkSchema,
+  ingest_link_data: IngestLinkSchema, conn: AsyncConnection
 ) -> Result[IngestLinkResponseSchema, str]:
   scraper_config = ScraperConfig(
-    max_depth=ingest_link_schema.max_depth, max_pages=ingest_link_schema.max_pages
+    max_depth=ingest_link_data.max_depth, max_pages=ingest_link_data.max_pages
   )
   scraper = DocumentationScraper(scraper_config)
 
   scrape_result: Result[Dict, str] = await scraper.scrape_website(
-    ingest_link_schema.url, ingest_link_schema.indexName
+    ingest_link_data.url, ingest_link_data.indexName
   )
   if isinstance(scrape_result, Err):
     return Err(scrape_result.err())
 
-  data_storage_result = await store_data(ingest_link_schema.indexName)
+  data_storage_result = await store_data(conn, ingest_link_data.indexName)
   if isinstance(data_storage_result, Err):
     return Err(data_storage_result.err())
 
@@ -55,10 +61,12 @@ async def _ingest_link(
 
 
 @router.post("/link", response_model=IngestLinkResponseSchema)
-async def ingest_link(ingest_link_schema: IngestLinkSchema):
+async def ingest_link(
+  ingest_link_data: IngestLinkSchema, conn: AsyncConnection = Depends(get_db_conn)
+):
   try:
     result: Result[IngestLinkResponseSchema, str] = await _ingest_link(
-      ingest_link_schema
+      ingest_link_data, conn
     )
     match result:
       case Ok(summary):
