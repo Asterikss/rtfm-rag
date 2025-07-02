3952af91a6a8cc5bee3b70f10d0ca7b98406f8f8
Asterikss <AndreAndreM@proton.me>
2025-07-02 20:46:24 +0200

    add insert_chunks to chunks respository and change how inserting single chunk is handled

--- src/repositories/chunk_repository.py
+++ src/repositories/chunk_repository.py
@@ -1,12 +1,15 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING, Tuple
 
 from result import Err, Ok, Result
 
+from ..models.models import ChunkData
+from ..rag.embedder import embed_data
+
 if TYPE_CHECKING:
-  import psycopg
   from psycopg import AsyncConnection
+  from openai import OpenAI
 
 
 @dataclass
@@ -17,7 +20,6 @@ class ChunkRetriveData:
   url: str
 
 
-# conn: psycopg.Connection, new_embedding: List[float], index_id: int, top_k: int = 10, conn =
 async def find_closest_chunks(
   conn: AsyncConnection,
   new_embedding: List[float],
@@ -50,20 +52,55 @@ async def find_closest_chunks(
     return Err(f"Exception in find_closest_chunks: {e}")
 
 
-def insert_chunk(
-  conn: psycopg.Connection,
+async def _bare_insert_chunk(
+  conn: AsyncConnection,
   content: str,
   embedding: List[float],
   url: str,
   index_id: int,
 ) -> Result[None, str]:
   try:
-    with conn.cursor() as cur:
-      cur.execute(
+    async with conn.cursor() as cur:
+      await cur.execute(
         "INSERT INTO chunks (content, embedding, url, index_id) VALUES (%s, %s, %s, %s)",
         (content, embedding, url, index_id),
       )
     return Ok(None)
   except Exception as e:
-    conn.rollback()
     return Err(f"Exception in insert_chunk: {e}")
+
+
+async def insert_chunks(
+  conn: AsyncConnection,
+  chunks: List[ChunkData],
+  openai_client: OpenAI,
+  index_id: int,
+) -> Result[Tuple[int, int], str]:
+  chunks_inserted = 0
+  chunks_failed = 0
+
+  try:
+    for chunk in chunks:
+      # TODO: batching
+      embedding_result: Result[List[float], str] = await embed_data(
+        openai_client, chunk.content
+      )
+      if isinstance(embedding_result, Err):
+        chunks_failed += 1
+        continue
+
+      insert_result: Result[None, str] = await _bare_insert_chunk(
+        conn, chunk.content, embedding_result.ok(), chunk.url, index_id
+      )
+      if isinstance(insert_result, Err):
+        chunks_failed += 1
+        continue
+
+      chunks_inserted += 1
+
+    await conn.commit()
+
+    return Ok((chunks_inserted, chunks_failed))
+  except Exception as e:
+    await conn.rollback()
+    return Err(f"Err in insert_chunks: {e}")
