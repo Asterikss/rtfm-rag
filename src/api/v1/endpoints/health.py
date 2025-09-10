c26afaa41d14df77ed29febfa082afc12e470c34
Asterikss <AndreAndreM@proton.me>
2025-09-10 17:47:59 +0200

    chore: health endpoint: change checkz back to root / and make the healthz more verbose; add assets/ to gitignore

--- src/api/v1/endpoints/health.py
+++ src/api/v1/endpoints/health.py
@@ -1,17 +1,25 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 from fastapi import APIRouter, Depends
 
 from ....core.config import config
 from ....services.database_service import get_db_conn
 
+if TYPE_CHECKING:
+  from psycopg import AsyncConnection
+
 router = APIRouter()
 
 
 @router.get("/healthz")
-async def healthz(conn=Depends(get_db_conn)):
-  await conn.execute("SELECT 1")
-  return {"status": "ok"}
+async def healthz(conn: AsyncConnection = Depends(get_db_conn)):
+  async with conn.cursor() as cursor:
+    await cursor.execute("SELECT 1")
+    result = await cursor.fetchone()
+  return {"status": "ok", "result": result}
 
 
-@router.get("/checkz")
+@router.get("/")
 async def read_root():
   return {f"{config.PROJECT_NAME}": "it is"}
